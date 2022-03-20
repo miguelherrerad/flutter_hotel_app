4cb1e5237eb81cd2466bd6003ae1ae8bb08cebdd
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-19 22:38:58 -0500

    Page View Controller

---
+++ lib/page_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageViewController extends StatefulWidget {
+  PageViewController({Key? key}) : super(key: key);
+
+  @override
+  State<PageViewController> createState() => _PageViewControllerState();
+}
+
+class _PageViewControllerState extends State<PageViewController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
