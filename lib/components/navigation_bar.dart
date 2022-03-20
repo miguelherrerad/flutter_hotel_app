e8471d7f9273fb095e151cd5151568f24c32d441
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-19 23:52:28 -0500

    Finish

--- lib/components/navigation_bar.dart
+++ lib/components/navigation_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class NavigationBar extends StatelessWidget {
   const NavigationBar({
     Key? key,
--- lib/components/search_field.dart
+++ lib/components/search_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class SearchField extends StatelessWidget {
   const SearchField({
     Key? key,
